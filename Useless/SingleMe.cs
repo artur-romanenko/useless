6827017b43e76badb3cdb7c94f297c579aa93d4c
arro <artur_romanenko@hotmail.com>
2018-12-23 15:34:05 +0200

    Add SingleMe class

---
+++ Useless/SingleMe.cs
@@ -0,0 +1,17 @@
+﻿namespace Useless
+{
+    /// <summary>
+    /// SingleMe classs.
+    /// </summary>
+    public class SingleMe
+    {
+        public static readonly SingleMe Instance = new SingleMe();
+
+        private SingleMe() { }
+
+        public override string ToString()
+        {
+            return "Single Me!";
+        }
+    }
+}
